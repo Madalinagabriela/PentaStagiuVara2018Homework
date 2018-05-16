2a2fa36eeb0f830f5741d9c57c7f051094f8be27
madalina popa <popa.gabrielamadalina@gmail.com>
2018-05-16 17:03:30 +0300

    Guess the number

---
+++ Module01Week01/Homework01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int guess = randomNumber.Next(1, 100);
+
+            Console.WriteLine("Introduceti un numar intre 1 si 100:");
+            tryAgain: int input = int.Parse(Console.ReadLine());
+
+            while (input != guess)
+            {
+                if (input < guess)
+                {
+                    Console.WriteLine("Numarul introdus este prea mic. Mai incearca!");
+                    goto tryAgain;
+                }
+                else
+                {
+                    Console.WriteLine("Numarul introdus este prea mare. Mai incearca!");
+                    goto tryAgain;
+                }
+            }
+
+            Console.WriteLine("Ati ghicit numarul!");
+            Console.ReadKey();
+        }
+    }
+}
